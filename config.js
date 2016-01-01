216e635145da6edc3448402b0ac663f1b5ab9bfb
zakauddin <m.zaka7@gmail.com>
2016-01-01 22:03:20 +0400

    adding first file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	
+	'port': process.env.PORT || 8080,
+	'database': 'mongodb://zakaud:meraaccount1@apollo.modulusmongo.net:27017/imOx2iny',
+	'secret': 'ilovescotchscotchyscotchscotch'
+};
